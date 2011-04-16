7e46621ddab2e6bf2e94e4afb2d5046ce63142b6
Erich Eichinger <eeichinger@gmail.com>
2011-04-16 19:31:05 -0400

    introduced junit formatter output to 'all' sample

--- samples/all/src/test/java/helloworld/IgnoredFeature.java
+++ samples/all/src/test/java/helloworld/IgnoredFeature.java
@@ -2,8 +2,10 @@ package helloworld;
 
 import org.junit.Ignore;
 import org.oaky.cuke4duke.FeatureConfiguration;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,11 +13,15 @@ import org.springframework.transaction.annotation.Transactional;
 @FeatureConfiguration("features/IgnoredFeature.feature")
 @ContextConfiguration("HelloWorldFeature-context.xml")
 @Transactional
-public class IgnoredFeature {
+public class IgnoredFeature implements BeanFactoryPostProcessor {
 
     @Autowired
     Object ms;
 
     public IgnoredFeature() {
+    }
+
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        throw new UnsupportedOperationException("spring should never instantiate even infrastructure classes from the --require path");
     }
 }
