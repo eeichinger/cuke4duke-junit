17d4bc374730d5cd43375428a47f9f88ce7e2c35
Erich Eichinger <eeichinger@gmail.com>
2011-04-19 19:06:21 -0400

    hardcode cuke4duke version number

--- junit-runner-jruby/src/main/resources-jruby/META-INF/jruby.gem.home/gems/cuke4duke-0.4.3/lib/cuke4duke/version.rb
+++ junit-runner-jruby/src/main/resources-jruby/META-INF/jruby.gem.home/gems/cuke4duke-0.4.3/lib/cuke4duke/version.rb
@@ -1,7 +1,8 @@
 require 'rexml/document'
 
 module Cuke4Duke
-  pom_version = REXML::XPath.first(REXML::Document.new(IO.read(File.dirname(__FILE__) + '/../../pom.xml')), '//xmlns:project/xmlns:version/text()').to_s
+#  pom_version = REXML::XPath.first(REXML::Document.new(IO.read(File.dirname(__FILE__) + '/../../pom.xml')), '//xmlns:project/xmlns:version/text()').to_s
+  pom_version = "0.4.3"
   if pom_version =~ /(.*)-SNAPSHOT$/
     VERSION = "#{$1}.beta"
   else
